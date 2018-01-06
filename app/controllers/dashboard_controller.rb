95389caccbcc97c76c940794159893887e15aa66
Enrique Florendo <enriqueflorendo@yahoo.com>
2018-01-05 21:51:39 -0800

    makes chart conditional on transactions

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,5 +4,6 @@ class DashboardController < ApplicationController
   def index
     @wallets = Wallet.where("user_id = #{current_user.id}")
     @servers = Server.where("user_id = #{current_user.id}")
+    @transactions = Transaction.where("user_id = #{current_user.id}")
   end
 end
